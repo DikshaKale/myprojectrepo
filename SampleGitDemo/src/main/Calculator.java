237d574111aca431f1b2d866f595ca9559db853c
Diksha <Diksha@LAPTOP-KLD9B4D7>
2021-03-23 10:55:04 +0530

    Added master

--- SampleGitDemo/src/main/Calculator.java
+++ SampleGitDemo/src/main/Calculator.java
@@ -37,7 +37,9 @@ public class Calculator {
 	      System.out.println(num1*num1*num1*num1);
 	  }
 
-	
+	public static void master1(int num1){
+	      System.out.println(num1*num1*num1*num1);
+	  }
 }
 
 
